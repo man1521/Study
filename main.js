0f8cd7fd16ea92dd629afce8f601213f15fac120
Mane Mkhitaryan <107885751+man1521@users.noreply.github.com>
2022-07-24 12:53:55 +0400

    film app
    film app

---
+++ main.js
@@ -0,0 +1,6 @@
+// var imgBg1 = document.getElementById('bg1');
+// imgBg1.src = 'bg2.jpg';
+var img = document.getElementById('bg1').src = 'bg2.jpg';
+document.title = " Hitman's Wife's Bodyguard";
+var div = document.querySelector('.main_promo');
+div.remove();
